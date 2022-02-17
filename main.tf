502b805e05d20211b9cd27d8b09ec69bd9084891
Tim Dockenfuß <tim.dockenfuss@axelspringer.com>
2022-02-17 10:50:22 +0000

    eip added

--- main.tf
+++ main.tf
@@ -17,11 +17,15 @@ variable "my_default_tags" {
 
 
 resource "aws_instance" "webserver" {
-  ami           = "ami-0cd855c8009cb26ef"
+  ami = data.aws_ami.amazon-linux-2.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.web.id]
   user_data = templatefile("user_data.sh", {username ="Kirk"})
   tags = merge(local.tags, tomap( {Name = "Tim's Webserver" , cost_center = "6310", stage = "test"}))
+
+}
+
+resource "aws_instance" "exam" {
 
 }
   
@@ -63,4 +67,14 @@ locals {
     Owner = "<XXX>"
   }
 }
- 
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+}
+resource "aws_eip" "example"{
+
+}
